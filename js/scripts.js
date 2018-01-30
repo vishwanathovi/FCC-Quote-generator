044908335b87f05a61310cb6604e9604a0f125e1
Vishwanatha HM <vishwanathovi@gmail.com>
2018-01-30 20:15:28 +0530

    modified jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 
-$(document).ready(function() {
-  $(".generate-btn").on('click',function(){
-    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-  $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
-    });
-  });
-});           
+  // $(".generate-btn").on('click',function(){
+  //   $.getJSON("https://cors-anywhere.herokuapp.com/https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(a) {
+  // $(".quote").text(a[0].content)
+  //   });
+  // });        
+
+
+
+  var API_URL = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+//Pull quote from API
+
+$(".generate-btn").on('click',function(){
+$.ajax({
+  url: API_URL, 
+  success: function(data){
+    var post = data.shift();
+    $(".author").text(post.title);
+    $(".quote").html(post.content);
+  },
+  cache:false
+});
+}); 
