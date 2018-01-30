5c057d9c29d6628964bfe4be554304d1c8e59e18
vishwanathovi <vishwanathovi@gmail.com>
2018-01-30 10:49:15 +0530

    adding the jquery part

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$('#generate-btn').on('click', function(e) {
+    e.preventDefault();
+    $.ajax( {
+      url: '/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        $('#quote').text(post.content);
+        // $('#quote').html(post.content);
+
+        // If the Source is available, use it. Otherwise hide it.
+        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+          $('#quote-source').html('Source:' + post.custom_meta.Source);
+        } else {
+          $('#quote-source').text('');
+        }
+      },
+      cache: false
+    });
+  });
+                     
