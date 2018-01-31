8d80582c844a87ab1025bc8f27ba856374a587e8
Vishwanatha HM <vishwanathovi@gmail.com>
2018-01-31 19:56:15 +0530

    31-Jan version

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,17 @@
   //   });
   // });        
 
+function stripHtml(html){
+    // Create a new div element
+    var temporalDivElement = document.createElement("div");
+    // Set the HTML content with the providen
+    temporalDivElement.innerHTML = html;
+    // Retrieve the text property of the element (cross-browser support)
+    return temporalDivElement.textContent || temporalDivElement.innerText || "";
+}
 
 
-  var API_URL = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var API_URL = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 //Pull quote from API
 
 $(".generate-btn").on('click',function(){
@@ -15,9 +23,18 @@ $.ajax({
   url: API_URL, 
   success: function(data){
     var post = data.shift();
-    $(".author").text(post.title);
-    $(".quote").html(post.content);
+    $(".author").text(stripHtml(post.title));
+    $(".quote").html(stripHtml(post.content));
   },
   cache:false
-});
+  });
+
+var oldUrl = $(".twitter-share-button").attr("href");
+var newUrl = "https://twitter.com/intent/tweet"
+var res = $(".quote").text();
+res = res.trim();
+var res = newUrl.concat("?text=",res);
+res = res.replace(/ /g, "%20");
+$(".twitter-share-button").attr("href",res);
+
 }); 
